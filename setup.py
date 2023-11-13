40c977e84732c21a848eea7a25d3efb8843b6c96
marzi <marzia.fazelian@fau.de>
2023-11-13 13:35:43 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     name='DSSS_Homework2',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-    ],
     author='Marzia Fazelian',
     author_email='Marzia.fazelian@fau.de',
     description='this is math quiz package',
